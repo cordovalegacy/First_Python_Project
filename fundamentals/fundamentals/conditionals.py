dc84dfe716f7fc2a261dc4aa729e1c7bd4972baf
Brendan Cordova <cordovalegacy19@gmail.com>
2022-11-21 09:08:47 -0800

    conditional practice

---
+++ fundamentals/fundamentals/conditionals.py
@@ -0,0 +1,17 @@
+# x = 12
+# if x > 50:
+#     print("bigger than 50")
+# else:
+#     print('smaller than 50')
+
+a = 55
+if a > 10:
+    print("bigger than 10")
+elif a > 50:
+    print('bigger than 50')
+else:
+    print('smaller than 10')
+
+# instead of && ... =>  and
+# instead of || ... =>  or
+# instead of !  ... =>  not
---
+++ fundamentals/fundamentals/tuples.py
@@ -0,0 +1,12 @@
+# TUPLE = IMMUTABLE LIST
+
+# tupples use parenthesis()
+# examples:
+tuple_data = ('physics', 'chemistry', 1997, 2000)
+tuple_num = (1, 2, 3, 4, 5 )
+tuple_letters = "a", "b", "c", "d"
+
+print(tuple_data)
+print(tuple_data + tuple_num)
+print(tuple_data + tuple_letters)
+print(tuple_data + tuple_letters + tuple_num)
