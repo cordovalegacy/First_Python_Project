407f1dcfc4b86fd36feba9328866cd9ddc24fecf
Brendan Cordova <cordovalegacy19@gmail.com>
2022-11-21 08:23:14 -0800

    list manipulation

--- fundamentals/fundamentals/lists.py
+++ fundamentals/fundamentals/lists.py
@@ -6,3 +6,27 @@ print(fruits_and_vegetables)
 
 salad = 3 * vegetables
 print(salad)
+
+drawers = ['documents', 'envelopes', 'pens']
+
+print(drawers[0])
+print(drawers[1])
+print(drawers[2])
+print(drawers)
+
+drawers[0] = 'socks'
+drawers[1] = 'shirts'
+drawers[2] = 'pants'
+print(drawers)
+
+drawers[1] = drawers[0]
+print(drawers)
+
+drawers.pop()
+print(drawers)
+
+drawers.append('ties')
+print(drawers)
+
+fruits.append(vegetables)
+print('Updated grocery list: ', fruits)
