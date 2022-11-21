9d04b6625bca337bdfb64649af1ae838d4cb4dcc
Brendan Cordova <cordovalegacy19@gmail.com>
2022-11-21 08:36:40 -0800

    slicing practice

--- fundamentals/fundamentals/lists.py
+++ fundamentals/fundamentals/lists.py
@@ -30,3 +30,29 @@ print(drawers)
 
 fruits.append(vegetables)
 print('Updated grocery list: ', fruits)
+
+fruits.extend(vegetables)
+print(fruits)
+
+words = ["start","going","till","the","end"]
+
+print(words[:1])
+print(words[1:])
+
+print(words[1:1])
+print(words[1:2])
+print(words[4:])
+print(words[4:1])
+print(words[2:4])
+
+# # Sub-ranges (portions) of the list
+# print(words[1:]) # prints ['going', 'till', 'the', 'end']
+# print(words[:4]) # prints ['start', 'going', 'till', 'the']
+# print(words[2:4]) # prints ['till', 'the']
+    
+# # Making a copy of a list
+# copy_of_words = words[:]
+# copy_of_words.append("dojo") # only appends to the copy
+# print(copy_of_words) # prints ['start', 'going', 'till', 'the', 'end', 'dojo']
+# print(words) # prints ['start', 'going', 'till', 'the', 'end']
+
