7ef81bfe645ce603143a78c0065f7715016f2f2c
Brendan Cordova <cordovalegacy19@gmail.com>
2022-11-21 18:43:23 -0800

    class, constructor, instances, setting attributes

---
+++ fundamentals/oop/setting_attributes.py
@@ -0,0 +1,22 @@
+class Shoe:
+
+    def __init__(self, brand, shoe_type, price):
+        self.brand = brand
+        self.type = shoe_type
+        self.price = price
+        self.in_stock = True
+
+
+skater_shoe = Shoe("Vans", "Low-top Trainers", 59.99)
+dress_shoe = Shoe("Jack & Jill Bootery", "Ballet Flats", 29.99)
+work_shoe = Shoe("Timberland", "Boots", "150.00")
+
+work_shoe.in_stock = 'False'
+
+print(skater_shoe.type)
+print(dress_shoe.type)
+print(work_shoe.type)
+print(work_shoe)
+print(work_shoe.in_stock)
+
+# Update the in_stock attribute to False
